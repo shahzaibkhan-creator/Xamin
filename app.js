7b31b0f5f375c860bb9c12a15ebf8458622fc2d7
shahzaibkhan-creator <mrmsha47@gmail.com>
2026-04-18 01:32:43 +0530

    adding app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {view,text} from 'react-native';
+
+export default function App() {
+  return(
+    <view style={{flex:1,justifycontent:'center',
+                 alignltems:'center'}}>
+<text>App is running </text>
+  </view>);
+}
